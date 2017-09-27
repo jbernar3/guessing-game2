282e7ef163e46d0689d77c7f04607276fc5d4754
BT_1N3_34 <BT_1N3_34@BT_1N3_34.bktech.local>
2017-09-27 12:56:01 -0400

    anotha comment

--- GuessingGameJeff/src/GuessingGame.java
+++ GuessingGameJeff/src/GuessingGame.java
@@ -1,6 +1,6 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
-// its me Usman juhurddd
+// its me Usman juhurdddd
 //hey
 public class GuessingGame {
 	public static void main (String[] args)
