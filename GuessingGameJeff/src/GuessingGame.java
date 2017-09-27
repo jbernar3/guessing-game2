dd9e0adc29eef8aff60ab7342cca7b7a8ba155fa
BT_1N3_33 <BT_1N3_33@BT_1N3_33.bktech.local>
2017-09-27 12:23:06 -0400

    new code

---
+++ GuessingGameJeff/src/GuessingGame.java
@@ -0,0 +1,41 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class GuessingGame {
+	public static void main (String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		int guess = 0;
+		int guessAmount = 1;
+		int answer = (int)(Math.random() * 10) + 1;
+		
+		while (guess != answer)
+		{
+			
+			boolean isNumber = false;
+			while (!isNumber)
+			{
+				try
+				{
+					System.out.println("print out you guess");
+					guess = input.nextInt();
+					isNumber = true;
+				}
+				catch (InputMismatchException e)
+				{
+					System.out.println("That was error: " + e);
+					System.out.println("Please try again");
+				}
+			}
+			if (guess != answer)
+			{
+				System.out.println("Wrong Number! try again");
+				guessAmount++;
+			}
+		}
+		System.out.println("Congratulations! You guessed the number in " + guessAmount + " tries");
+		System.out.println("If you want to play again say \"yes\"");
+		
+	}
+
+}
